e7d744ece0c059709724781bf2a71674f6a8bb18
Tio Yudho Prasetio <tioyudhoprasetio33@gmail.com>
2026-07-26 13:31:59 +0700

    fix: remove SPA rewrite in vercel.json, untrack tsconfig.tsbuildinfo, and set force-dynamic on edit page

--- src/app/dashboard/transaction/edit/[id]/page.tsx
+++ src/app/dashboard/transaction/edit/[id]/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+export const dynamic = "force-dynamic";
+
 import TransactionForm from "@/pages/TransactionForm";
 import { fetchTransactionById, editTransaction } from "@/services/transaction";
 import { useParams, useRouter } from "next/navigation";
